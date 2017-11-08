d5f64c51ee86abef6e6853b01db4785f067df1c7
lukas2005 <lukas2005.38@gmail.com>
2017-11-08 15:47:27 +0100

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/pi/projects/Video-Uploader-Bot/
-mvn clean install
-java -jar /home/pi/projects/Video-Uploader-Bot/target/videobot-1.0-SNAPSHOT.jar
+#cd /home/pi/projects/Video-Uploader-Bot/
+#mvn clean install
+#java -jar /home/pi/projects/Video-Uploader-Bot/target/videobot-1.0-SNAPSHOT.jar
+mvn exec:java -Dexec.mainClass="io.github.videobot.Main" -Dexec.classpathScope=runtime
