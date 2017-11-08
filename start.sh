f0deb92be417db635dd71481353fbbc5598829a5
lukas2005 <lukas2005.38@gmail.com>
2017-11-08 15:02:23 +0100

    maven conf

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/projects/Video-Uploader-Bot/
 mvn clean install
-java -jar target/videobot-1.0-SNAPSHOT.jar
+java -jar /home/pi/projects/Video-Uploader-Bot/target/videobot-1.0-SNAPSHOT.jar
