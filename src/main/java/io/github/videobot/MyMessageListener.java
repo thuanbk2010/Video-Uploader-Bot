3e6a399a78b294a109814362f527e30a0386654d
lukas2005 <lukas2005.38@gmail.com>
2017-11-10 14:02:44 +0100

    changed the max filesize

--- src/main/java/io/github/videobot/MyMessageListener.java
+++ src/main/java/io/github/videobot/MyMessageListener.java
@@ -42,8 +42,8 @@ public class MyMessageListener implements MessageCreateListener, MessageEditList
                             URLConnection urlConnection = url.openConnection();
                             urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
                             urlConnection.connect();
-                            if (urlConnection.getInputStream().available() > 10000000) {
-                                fileProcessMessage.edit("File is too big max file size = 10 MB");
+                            if (urlConnection.getInputStream().available() > 50000000) {
+                                fileProcessMessage.edit("File is too big max file size = 50 MB");
                                 throw new Exception("ignore");
                             }
                             message.delete();
